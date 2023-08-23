c8b2bf47f479f3aeaf620b4e8ee700ab3274ad7f
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-08-23 04:55:49 -0300

    Criação do componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  return (<p>Sistema Solar</p>);
+  return (
+    <>
+      <p>Sistema Solar</p>
+      <Header />
+    </>
+  );
 }
 
 export default App;
